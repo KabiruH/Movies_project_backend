e59061077bc2fd495101d9b8355728b6d8eef5de
King <kelkab46@gmail.com>
2023-03-04 10:10:08 +0300

    started backend

---
+++ db/migrate/20230302105258_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+
+    create_table :movies do |t|
+      t.string :title
+      t.integer :year
+      t.string :description
+      t.integer :user_id
+    end
+
+  end
+end
